9ec93385da8530622c65c8f120f616995c99b59f
Devanshu <135867194+DPandaman@users.noreply.github.com>
2025-08-14 18:47:12 -0500

    download.py

--- src/download.py
+++ src/download.py
@@ -1,3 +1,4 @@
 Files download link: https://pypi.org/project/tldrcli/
-Website description is slightly outdated. Will be updated with v0.2 -> v1.2 release 
+Website description is slightly outdated. ̶W̶i̶l̶l̶ ̶b̶e̶ ̶u̶p̶d̶a̶t̶e̶d̶ ̶w̶i̶t̶h̶ ̶v̶0̶.̶2̶ ̶-̶>̶ ̶v̶1̶.̶2̶ ̶r̶e̶l̶e̶a̶s̶e̶  Fixed with v1.3.1 release 
 Install tldr directly in your terminal with: pip install tldrcli
+If you already have tldr but not the latest version, update it with pip install --upgrade tldrcli
